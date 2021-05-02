dcebc6c8006a59e3de86db30b8b1c14c8150e203
ObonyoCynthia <cynthia.obonyo@student.moringaschool.com>
2021-05-02 23:01:18 +0300

    added shed ids

--- dairy.js
+++ dairy.js
@@ -16,12 +16,18 @@ function productioPerDay(){
     var shedDcows= parseInt(document.getElementById('dcows').value);
     var shedDmilk= parseInt(document.getElementById('dmilk').value);
     var shedDmilk=( shedDcows*shedDmilk) 
+
+    document.getElementById('shedA').innerHTML = "Your production in Shed A " + shedAmilk+" litres per day";
+    document.getElementById('shedB').innerHTML = "Your production in Shed B " + shedAmilk+" litres per day";
+    document.getElementById('shedC').innerHTML = "Your production in Shed C " + shedAmilk+" litres per day";
+    document.getElementById('shedD').innerHTML = "Your production in Shed D " + shedAmilk+" litres per day";
+  
     // alert( "Your production in Shed A " + shedAmilk+" litres per day")
     // alert( "Your production in Shed B " + shedBmilk+" litres per day")
     // alert( "Your production in Shed C " + shedCmilk+" litres per day")
     // alert( "Your production in Shed D " + shedDmilk+" litres per day")
-    var totallittresperday= (shedAmilk+ shedBmilk +shedCmilk+ shedDmilk)
-    alert (totallittresperday)
+    var totallittresperday= (shedAmilk + shedBmilk + shedCmilk + shedDmilk)
+    console.log (totallittresperday)
       
     var months={
         "January" :31,
@@ -41,13 +47,13 @@ function productioPerDay(){
     sum = 0
     
     Object.entries(months).forEach(([key, value]) => {
-        alert('Your income for ' +key+ ' is ' + (value * totallittresperday * 45) + ' ksh');
+        console.log('Your income for ' +key+ ' is ' + (value * totallittresperday * 45) + ' ksh');
         document.getElementById('monthlyincome').innerHTML += 'Your income for ' +key+ ' is ' + (value * totallittresperday * 45) + ' ksh';
         
         sum += (value * totallittresperday * 45)
         
     });
 
-    alert('Income for the year is ' + sum)
+    console.log('Income for the year is ' + sum)
     
 }
