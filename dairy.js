09f3bb584df98dd3c25c971b27f4a7f978919692
ObonyoCynthia <cynthia.obonyo@student.moringaschool.com>
2021-05-02 14:18:42 +0300

    created input colums for sheds

---
+++ dairy.js
@@ -0,0 +1,7 @@
+function productioPerDay(){
+    var shedAcows= parseInt(document.getElementById('acows').value);
+    var shedAmilk= parseInt(document.getElementById('amilk').value);
+    var shedAmilk=( shedAcows*shedAmilk)
+    alert(shedAmilk)
+    
+}
